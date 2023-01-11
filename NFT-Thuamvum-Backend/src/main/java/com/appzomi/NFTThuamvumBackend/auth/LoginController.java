3b42b39511d834ef104814b60b323fa0e6dc0707
johnpiapian <johnpiapian@gmail.com>
2023-01-10 21:32:47 -0600

    Set up basic authentication

---
+++ NFT-Thuamvum-Backend/src/main/java/com/appzomi/NFTThuamvumBackend/auth/LoginController.java
@@ -0,0 +1,26 @@
+package com.appzomi.NFTThuamvumBackend.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/auth")
+public class LoginController {
+
+    private final LoginService loginService;
+
+    @Autowired
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    @PostMapping("login")
+    public Optional<String> login(@RequestBody LoginDTO loginForm){
+        return loginService.authenticateUser(loginForm);
+    }
+}
